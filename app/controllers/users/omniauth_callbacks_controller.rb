062b93748f79cdfd9b4c94f8169ef0149ff635b0
CONLIN MCMANUS <conlinmcmanus@gmail.com>
2017-04-04 15:57:30 -0500

    updated

--- app/controllers/users/omniauth_callbacks_controller.rb
+++ app/controllers/users/omniauth_callbacks_controller.rb
@@ -5,7 +5,7 @@ class Users::OmniauthCallbacksController < Devise::OmniauthCallbacksController
         @user = User.find_for_oauth(env["omniauth.auth"], current_user)
 
         if @user.persisted?
-          flash[:notice] = I18n.t "devise.omniauth_callbacks.success", kind: "#{provider}".capitalize)
+          flash[:notice] = I18n.t "devise.omniauth_callbacks.success", kind: "#{provider}".capitalize
           sign_in_and_redirect @user, event: :authentication
         else
           session["devise.#{provider}_data"] = request.env["omniauth.auth"].except(:extra)
