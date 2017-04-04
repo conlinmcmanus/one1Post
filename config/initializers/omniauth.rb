207682085aebb30941a52bba847ccdae5f9a6354
CONLIN MCMANUS <conlinmcmanus@gmail.com>
2017-04-04 16:26:53 -0500

    removed oauth initilizer

--- config/initializers/omniauth.rb
+++
@@ -1,6 +0,0 @@
-Rails.application.config.middleware.use OmniAuth::Builder do
-  provider :facebook, ENV['facebook_key'], ENV['facebook_secret']
-  provider :twitter, ENV['twitter_key'], ENV['twitter_secret']
-  provider :linkedin, ENV['linkedin_key'], ENV['linkedin_secret'], scope: 'r_basicprofile r_emailaddress w_share'
-  provider :google_oauth2, ENV['google_key'], ENV['google_secret'], name: 'google', scope: 'email, profile, https://plus.google.com/', prompt: 'select_account'
-end
