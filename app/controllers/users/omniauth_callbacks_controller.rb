1ab2e5ac48e914df7f91d593575f2e1e2bead380
CONLIN MCMANUS <conlinmcmanus@gmail.com>
2017-04-04 16:38:26 -0500

    fixed another error

--- app/controllers/users/omniauth_callbacks_controller.rb
+++ app/controllers/users/omniauth_callbacks_controller.rb
@@ -2,7 +2,7 @@ class Users::OmniauthCallbacksController < Devise::OmniauthCallbacksController
   def self.provides_callback_for(provider)
     class_eval %{
       def #{provider}
-        @user = User.find_for_oauth(env["omniauth.auth"], current_user)
+        @user = User.from_omniauth(env["omniauth.auth"], current_user)
 
         if @user.persisted?
           flash[:notice] = I18n.t "devise.omniauth_callbacks.success", kind: "#{provider}".capitalize
@@ -17,15 +17,5 @@ class Users::OmniauthCallbacksController < Devise::OmniauthCallbacksController
 
   %w[twitter facebook linkedin google_oauth2].each do |provider|
     provides_callback_for provider
-  end
-
-  def self.from_omniauth(access_token)
-    data = access_token.info
-    user = User.where(email: data['email']).first
-
-    unless user
-      user = User.create(name: data['name'], email: data['email'], password: Devise.friendly_token[0, 20])
-    end
-    user
   end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -3,11 +3,21 @@ class User < ApplicationRecord
   # :lockable, :timeoutable
   devise :database_authenticatable, :registerable, :confirmable, :recoverable, :rememberable, :trackable, :validatable, :omniauthable, omniauth_providers: %i[facebook twitter linkedin google_oauth2]
 
-  def self.from_omniauth(auth)
-    where(provider: auth.provider, uid: auth.uid).first_or_create do |user|
-      user.email = auth.info.email
-      user.password = Devise.friendly_token[0, 20]
+  # def self.from_omniauth(auth)
+  #   where(provider: auth.provider, uid: auth.uid).first_or_create do |user|
+  #     user.email = auth.info.email
+  #     user.password = Devise.friendly_token[0, 20]
+  #   end
+  # end
+
+  def self.from_omniauth(access_token)
+    data = access_token.info
+    user = User.where(email: data['email']).first
+
+    unless user
+      user = User.create(name: data['name'], email: data['email'], password: Devise.friendly_token[0, 20])
     end
+    user
   end
 
   has_many :posts
