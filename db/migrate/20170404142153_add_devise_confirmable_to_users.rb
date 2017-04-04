f2cf8d45a056cb2b9aad3cd791336a4c949106be
CONLIN MCMANUS <conlinmcmanus@gmail.com>
2017-04-04 09:28:11 -0500

    added confirmable to users table

---
+++ db/migrate/20170404142153_add_devise_confirmable_to_users.rb
@@ -0,0 +1,10 @@
+class AddDeviseConfirmableToUsers < ActiveRecord::Migration[5.0]
+  def change
+      # Confirmable
+      add_column :users, :confirmation_token, :string
+      add_column :users, :confirmed_at, :datetime
+      add_column :users, :confirmation_sent_at, :datetime
+      add_column :users, :unconfirmed_email, :string
+    end
+  end
+end
